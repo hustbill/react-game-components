936466fd1e233d3c80cfcfaf01fe2eb521536d3d
hua zhang <zhahua@microsoft.com>
2021-04-10 18:11:06 -0700

    useFetch hook for fetch api

--- react-ping-pang/src/App.js
+++ react-ping-pang/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import GameBoard from './components/GameBoard';
 
 import useForm from './hooks/useForm';
+import useFetch from './hooks/useFetch';
 import Hello from './components/Hello';
 
 function expensiveInitialState() {
@@ -14,16 +15,32 @@ function expensiveInitialState() {
 const App = () => {
   const [count, setCount] = useState(() => expensiveInitialState());
   const [count2, setCount2] = useState(20);
-  const [count3, setCount3] = useState(30);
   const [values, handleChange] = useForm({
     email: '',
     password: '',
     firstName: ''
   });
+  
   const [showHello, setShowHello] = useState(false);
 
+  // useEffect(() => {
+  //   console.log("render");
+  //   const onMouseMove = e => {
+  //     console.log(e);
+  //   }
+  //   window.addEventListener('mousemove', onMouseMove)
+
+  //   return () => {
+  //     //  console.log('cleanup');
+  //     window.removeEventListener('mousemove', onMouseMove);
+  //   }
+  // }, []);
+
+  // fetch api: numbersapi.com/43/trivia
+  const { data, loading } = useFetch("http://numbersapi.com/43/trivia");
   return (
     <div className="App">
+             <div>{loading ? "loading..." : data}</div>
       <div>
         <h3>Ping Pang Game using React</h3>
         <h4>
@@ -34,19 +51,18 @@ const App = () => {
             rel="noopener noreferrer"
           >Demo Source </a>
         </h4>
+ 
 
         <div>
           <button
             onClick={() => {
               setCount(c => c + 1);
               setCount2(c => c + 1);
-              setCount3(c => c + 1);
             }} >
             +
           </button>
           <div>count 1: {count} </div>
-          {/* <div>count 2: {count2} </div>
-          <div>count 3: {count3} </div> */}
+          <div>count 2: {count2} </div>
 
           {/* <GameBoard /> */}
         </div>
