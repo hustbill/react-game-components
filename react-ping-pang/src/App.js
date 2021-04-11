afc041b3882f736e24cd283072ba03398e3789cf
hua zhang <zhahua@microsoft.com>
2021-04-10 17:06:06 -0700

    use input componet, email and password

--- react-ping-pang/src/App.js
+++ react-ping-pang/src/App.js
@@ -1,11 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
 import GameBoard from './components/GameBoard';
 
-function App() {
+function expensiveInitialState() {
+  return 10;
 
+}
+const App = () => {
+  const [count, setCount] = useState(() => expensiveInitialState());
+  const [count2, setCount2] = useState(20);
+  const [count3, setCount3] = useState(30);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="App">
@@ -22,11 +30,36 @@ function App() {
         </h4>
 
         <div>
-          <GameBoard />
-        </div>
+          <button
+            onClick={() => {
+              setCount(c => c + 1);
+              setCount2(c => c + 1);
+              setCount3(c => c + 1);            
+          }} >
+            +
+          </button>
+        <div>count 1: {count} </div>
+        <div>count 2: {count2} </div>
+        <div>count 3: {count3} </div>
 
+        {/* <GameBoard /> */}
       </div>
+      <div>
+        <input
+          name="email"
+          value={email}
+          onChange={ e => setEmail(e.target.value)}
+        />
+        <input 
+          type="password"
+          name="password"
+          value={password}
+          onChange={ e => setPassword(e.target.value)}
+          />
+      </div>
+
     </div>
+    </div >
   );
 }
 
